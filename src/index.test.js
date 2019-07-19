85168eda32bc85e6f292dbbb5d21f18d52b0de1a
Linesh <linesh.simpcity@gmail.com>
2019-07-19 13:46:04 +0800

    feature: implement second rule: two equals

--- src/index.test.js
+++ src/index.test.js
@@ -29,7 +29,7 @@ describe('奖励规则', () => {
     expect(result).toEqual(0)
   })
 
-  it.skip('规则二：两个相同：如果有两个值相同，奖励这两个数的总和', () => {
+  it('规则二：两个相同：如果有两个值相同，奖励这两个数的总和', () => {
     const numbers = [1, 2, 2, 3, 4]
 
     const result = new TwoEqualsRule(numbers).calculate()
@@ -37,7 +37,7 @@ describe('奖励规则', () => {
     expect(result).toEqual(4)
   })
 
-  it.skip('规则二：两个相同：如果有多于两个值相同，仅奖励其中两个数的总和', () => {
+  it('规则二：两个相同：如果有多于两个值相同，仅奖励其中两个数的总和', () => {
     const numbers = [1, 2, 2, 2, 3]
 
     const result = new TwoEqualsRule(numbers).calculate()
@@ -45,12 +45,20 @@ describe('奖励规则', () => {
     expect(result).toEqual(4)
   })
 
-  it.skip('规则二：两个相同：如果有多于两组值相同，奖励最大一组两个数的总和', () => {
+  it('规则二：两个相同：如果有多于两组值相同，奖励最大一组两个数的总和', () => {
     const numbers = [1, 2, 2, 4, 4]
 
     const result = new TwoEqualsRule(numbers).calculate()
 
     expect(result).toEqual(8)
+  })
+
+  it('规则二：两个相同：如果没有两个以上相同的值，则不奖励', () => {
+    const numbers = [1, 2, 3, 4, 5]
+
+    const result = new TwoEqualsRule(numbers).calculate()
+
+    expect(result).toEqual(0)
   })
 })
 
--- src/rules/two-equals-rule.js
+++ src/rules/two-equals-rule.js
@@ -1,7 +1,27 @@
 import { Rule } from './index'
 
+// https://stackoverflow.com/questions/840781/get-all-non-unique-values-i-e-duplicate-more-than-one-occurrence-in-an-array
+function findDuplicates(numbers) {
+  const uniq = numbers
+    .map((number) => ({
+      count: 1,
+      name: number,
+    }))
+    .reduce((result, b) => {
+      result[b.name] = (result[b.name] || 0) + b.count
+      return result
+    }, {})
+
+  return Object.keys(uniq).filter((a) => uniq[a] > 1)
+}
 export class TwoEqualsRule extends Rule {
   calculate() {
-    return this.numbers
+    const duplicates = findDuplicates(this.numbers)
+
+    if (duplicates.length > 0) {
+      return Number(Math.max(...duplicates)) * 2
+    }
+
+    return 0
   }
 }
