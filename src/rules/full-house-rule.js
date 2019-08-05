9af7a556378adfa62c7a64ab4fa195547337fd4e
Linesh <linesh.simpcity@gmail.com>
2019-08-05 22:59:55 +0800

    ♻️refactor: clean up

--- src/rules/full-house-rule.js
+++ src/rules/full-house-rule.js
@@ -2,19 +2,17 @@ import { SameNumbersRule } from './internal_same-numbers-rule'
 
 export class FullHouseRule extends SameNumbersRule {
   calculate() {
-    const threeOfAKind = this.findExactDuplicates(3)
-    const aPair = this.findExactDuplicates(2)
+    const [threeOfAKind] = this.findExactDuplicates(3)
+    const [aPair] = this.findExactDuplicates(2)
 
-    if (threeOfAKind.length === 0 || aPair.length === 0) {
+    if (!threeOfAKind || !aPair) {
       return 0
     }
 
-    const [three] = threeOfAKind
-    const [two] = aPair
-    if (three === two) {
+    if (threeOfAKind === aPair) {
       return 0
     }
 
-    return Number(three) * 3 + Number(two) * 2
+    return Number(threeOfAKind) * 3 + Number(aPair) * 2
   }
 }
